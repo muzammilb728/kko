58fbab157c1ba24735e7787b3b70274d3dce539c
muzammil ali <151667804+muzammilb728@users.noreply.github.com>
2024-02-06 08:32:17 -0800

    Add files via upload

---
+++ routine.py
@@ -0,0 +1,24 @@
+#5.
+# Time Management Tools
+taska_name = "assignment"
+task1_duration = 3.5  # in hours
+
+taskb_name = "presentation"
+task2_duration = 2.0  # in hours
+
+taskc_name = "shopping"
+task3_duration = 1.5  # in hour 
+# Calculate total time spent on each task
+total_task1_time = task1_duration
+total_task2_time = task2_duration
+total_task3_time = task3_duration
+
+# Calculate overall total time
+total_time = total_task1_time + total_task2_time + total_task3_time
+#result 
+print(f"Total time spent on {taska_name}: {total_task1_time} hours")
+print(f"Total time spent on {taskb_name}: {total_task2_time} hours")
+print(f"Total time spent on {taskc_name}: {total_task3_time} hours")
+
+print(f"\nOverall total time spent: {total_time:.2f} hours")
+
